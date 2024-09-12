7e469b1c398732deaed500ae402fddf606f60474
ChandanKrv <chandan.nit.cse@gmail.com>
2024-09-12 15:09:44 +0530

    initial commit

--- src/LogicBuilding/_1_AddElementInArray.java
+++ src/LogicBuilding/_1_AddElementInArray.java
@@ -11,25 +11,9 @@ public class _1_AddElementInArray {
     public static void main(String[] args) {
         int[] arr = {5, 10, 15, 20};
 
-        System.out.println("Old Array: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
-        int[] newArr = new int[arr.length + 1];
-        for (int i = 0; i < arr.length; i++) {
-            newArr[i] = arr[i];
-        }
-        newArr[newArr.length - 1] = 25;
-
-//        System.out.println("\nNew Array:");
-//        for (int i = 0; i < newArr.length; i++) {
-//            System.out.print(newArr[i] + " ");
-//        }
+        System.out.println("Array: " + Arrays.toString(arr));
 
 
-        System.out.println(Arrays.toString(newArr));
 
     }
 }
